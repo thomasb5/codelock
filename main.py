5a029ea537aadaef1c487e3f6957be41bf1ce4a5
Thomas Byrne <thomas5byrne5@gmail.com>
2023-10-11 10:37:15 -0400

    testing123

--- main.py
+++ main.py
@@ -23,3 +23,89 @@ with open("odd_numbers.txt", "w") as file:
 #testing testing
 #testing testing
 #testing testing#testing testing
+
+
+
+"""hello
+
+line3
+
+hello1"""
+
+print("hello!")
+
+with open("odd_numbers.txt", "w") as file:
+    number = 1
+
+    while number <= 1000:
+        if number % 2 != 0:
+            # Write the odd number to the file
+            file.write(str(number) + "\n")
+        number += 1
+
+
+#testing testing
+#testing testing
+#testing testing
+
+#testing testing
+#testing testing
+#testing testing#testing testing
+
+
+
+"""hello
+
+line3
+
+hello1"""
+
+print("hello!")
+
+with open("odd_numbers.txt", "w") as file:
+    number = 1
+
+    while number <= 1000:
+        if number % 2 != 0:
+            # Write the odd number to the file
+            file.write(str(number) + "\n")
+        number += 1
+
+
+#testing testing
+#testing testing
+#testing testing
+
+#testing testing
+#testing testing
+#testing testing#testing testing
+
+
+
+"""hello
+
+line3
+
+hello1"""
+
+print("hello!")
+
+with open("odd_numbers.txt", "w") as file:
+    number = 1
+
+    while number <= 1000:
+        if number % 2 != 0:
+            # Write the odd number to the file
+            file.write(str(number) + "\n")
+        number += 1
+
+
+#testing testing
+#testing testing
+#testing testing
+
+#testing testing
+#testing testing
+#testing testing#testing testing
+
+
